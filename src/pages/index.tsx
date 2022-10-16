bdad113060e2d108e11ff18e757ebfa94e3cba9f
Aryella Lacerda <aryella.lacerda@gmail.com>
2022-10-16 16:06:20 -0300

    chore: initial personalizations to project structure & config

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="h-screen bg-gradient-to-r from-green-500 to-blue-500">
+      <h1>Admin</h1>
+    </div>
+  );
+};
+
+export default Home;
