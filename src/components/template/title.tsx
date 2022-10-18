baa88e5ae1fae70bf20f4bd6a06075c93200258d
Aryella Lacerda <aryella.lacerda@gmail.com>
2022-10-18 14:37:10 -0300

    refactor: style the Title component

--- src/components/template/title.tsx
+++ src/components/template/title.tsx
@@ -6,8 +6,12 @@ type Props = {
 export const Title = ({ title, subtitle }: Props) => {
   return (
     <div>
-      <h1>{title}</h1>
-      <h2>{subtitle}</h2>
+      <h1 className="font-black text-3xl text-gray-800 dark:text-gray-200">
+        {title}
+      </h1>
+      <h2 className="font-light text-sm text-gray-700 dark:text-gray-300">
+        {subtitle}
+      </h2>
     </div>
   );
 };
